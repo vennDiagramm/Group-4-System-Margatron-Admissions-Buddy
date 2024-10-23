d8f64086c3098825dad1ee3d79f85dcff7182224
Marga Pilapil <margagpilapil@gmail.com>
2024-10-23 10:56:50 +0800

    keywords

--- mainGUI.py
+++ mainGUI.py
@@ -25,7 +25,9 @@ genai.configure(api_key=api_key)
 
 # Keywords for conversation
 GREETING_KEYWORDS = ["hi", "hello", "hey", "greetings", "whats up", "what's up", "yo", "how are you", "how are you doing"]
-ACCEPTED_KEYWORDS = ["payment methods", "admissions", "requirements", "tuition fees", "enroll", "school year", "scholarships", "apply", "enrollment", "application", "pay", "departments", "colleges", "SHS", "JHS", "College programs", "courses", "junior high school", "senior high school"]
+ACCEPTED_KEYWORDS = ["payment methods", "admissions", "requirements", "tuition fees", "enroll", "school year", "scholarships", 
+                     "apply", "enrollment", "application", "pay", "departments", "colleges", "shs", "jhs", "college programs", 
+                     "courses", "junior high school", "senior high school", "ccis", "cea","atycb","cas","chs", "college"]
 GOODBYE_KEYWORDS = ["thank you", "goodbye", "farewell"]
 
 # Connect to SQLite database and fetch the raw data
