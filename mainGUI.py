cc6e8392b63c15af495a64b3017aa18cf902c934
Marga Pilapil <margagpilapil@gmail.com>
2024-10-23 10:29:56 +0800

    will this work

--- mainGUI.py
+++ mainGUI.py
@@ -153,7 +153,7 @@ def main():
     st.write("Hello, how may I help you?")
 
     # Provide the path to your database file here
-    db_path = "old_files/database.db"  # This is the SQLite database path
+    db_path = "database/database1.db"  # This is the SQLite database path
     handle_conversation(db_path)
 
 
