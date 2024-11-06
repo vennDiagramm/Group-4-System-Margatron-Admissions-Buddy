9a4bc5a7387c555bbbcb9f6e8db7f9b296fae8c9
marga <margsmay23@gmail.com>
2024-11-06 14:56:40 +0800

    kalimot kog chage sa path

--- mainGUI.py
+++ mainGUI.py
@@ -146,7 +146,7 @@ def main():
     st.write("Hello, how may I help you?")
 
     # Provide the path to your database file here
-    db_path = "\\database/databasefinalnjud.db"  # This is the SQLite database path
+    db_path = "database/databasefinalnjud.db"  # This is the SQLite database path
     handle_conversation(db_path)
 
 
