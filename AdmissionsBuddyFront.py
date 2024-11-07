383386f052634a63ba015bbd258f1b9ec7dd18e5
ven <margsmay23@gmail.com>
2024-11-07 21:51:29 +0800

    update

--- AdmissionsBuddyFront.py
+++ AdmissionsBuddyFront.py
@@ -3,7 +3,7 @@ import AdmissionsBuddyBack as ABB
 # Function to handle GUI
 def main():
     # Streamlit set up
-    ABB.st.set_page_config(page_title="Margatron", page_icon="Icons/mapua_icon_83e_icon.ico")  # pwde nato e himo as mmcm logo
+    ABB.st.set_page_config(page_title="Margatron", page_icon="Icons/mapua_icon_83e_icon.ico")
     ABB.st.title("Margatron, Admissions Buddy :books:")
     ABB.st.write("Hello, how may I help you?")
 
