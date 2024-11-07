57e6e2f53f7f75de8c572999b91e51c14783ee6f
ven <margsmay23@gmail.com>
2024-11-07 22:06:15 +0800

    hi

--- AdmissionsBuddyFront.py
+++ AdmissionsBuddyFront.py
@@ -1,12 +1,11 @@
-import streamlit as st
 import AdmissionsBuddyBack as ABB
 
 # Function to handle GUI
 def main():
     # Streamlit set up
-    st.set_page_config(page_title="Margatron", page_icon="Icons/mapua_icon_83e_icon.ico")
-    st.title("Margatron, Admissions Buddy :books:")
-    st.write("Hello, how may I help you?")
+    ABB.st.set_page_config(page_title="Margatron", page_icon="Icons/mapua_icon_83e_icon.ico")
+    ABB.st.title("Margatron, Admissions Buddy :books:")
+    ABB.st.write("Hello, how may I help you?")
 
     # Provide the path to your database file here
     db_path = "database/databasefinalnjud.db"  # This is the SQLite database path
