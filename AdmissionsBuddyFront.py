dad51d892b5979e7ab2366a0437c7040a09ccc2a
ven <margsmay23@gmail.com>
2024-11-07 21:46:29 +0800

    updates

---
+++ AdmissionsBuddyFront.py
@@ -0,0 +1,17 @@
+import AdmissionsBuddyBack as ABB
+
+# Function to handle GUI
+def main():
+    # Streamlit set up
+    ABB.st.set_page_config(page_title="Margatron", page_icon="Icons/mapua_icon_83e_icon.ico")  # pwde nato e himo as mmcm logo
+    ABB.st.title("Margatron, Admissions Buddy :books:")
+    ABB.st.write("Hello, how may I help you?")
+
+    # Provide the path to your database file here
+    db_path = "database/databasefinalnjud.db"  # This is the SQLite database path
+    ABB.handle_conversation(db_path)
+
+
+# To run main   
+if __name__ == "__main__":
+    main()
